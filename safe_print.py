5d624b67ed2863a7919b11bfaaf3b93eaa580c81
Ben Grande <ben.grande.b@gmail.com>
2025-01-21 18:48:10 +0100

    Documentation
    Also test cases that failed and lead to regex improvements.

--- safe_print.py
+++ safe_print.py
@@ -4,91 +4,192 @@
 ##
 ## SPDX-License-Identifier: TODO
 
-## https://en.wikipedia.org/wiki/ANSI_escape_code#SGR
-
 """
-Sanitize text to only print ASCII and a subset of ANSI SGR
+Sanitize text to be safely printed on the terminal.
 """
 
 import argparse
 import re
-from typing import Optional, List
+from typing import Optional, Pattern
 
 
-## TODO: this function should only add the exclude codes which would match,
-## The codes that don't match should be returned to be later added by extended
-## color codes 8bit and 24bit.
-def exclude_color(original_pattern: str, exclude_codes: List[str]) -> str:
+def exclude_pattern(original_pattern: str,
+                    negate_pattern: list[str]) -> str:
+    """Exclude matching next expression if provided expression matches.
+
+    Parameters
+    ----------
+    original_pattern : str
+        Pattern which contains unwanted matches.
+
+    negate_pattern : list[str]
+        Pattern to exclude from the next match.
+
+    Returns
+    -------
+        str: regular expression with negative lookahead
+
+    Examples
+    --------
+    Redact unsafe sequences by default:
+    >>> color_pattern = exclude_pattern(r"(30|31)", ["31"])
+    '(?!(?:31))(30|31)'
     """
-    TODO
-    """
-    #exclude_pattern = r"(?!{})".format(exclude_codes)
-    #return r"{}{}".format(exclude_pattern, original_pattern)
-    exclude_codes_str = '|'.join(map(re.escape, exclude_codes))
-    exclude_pattern = r"(?!(?:{}))".format(exclude_codes_str)
-    return r"{}{}".format(exclude_pattern, original_pattern)
+
+    exclude_pattern_str = "|".join(map(re.escape, negate_pattern))
+    exclude_pattern_str = rf"(?!(?:{exclude_pattern_str}))"
+    return rf"{exclude_pattern_str}{original_pattern}"
 
 
-# pylint: disable=too-many-locals
-def safe_print(text: str, colors: bool = True, extra_colors: bool = True,
-               exclude_colors: Optional[List[str]] = None) -> str:
+def get_color_pattern(extra_colors: Optional[bool],
+                      exclude_colors: Optional[list[str]]
+                      ) -> Pattern[str]:
+    """Print compiled RegEx for SGR sequences
+
+    The SGR implementation is well documented[1][2][3] but the overall
+    structure of escape sequences syntax isn't, they have a permissive syntax
+    and that part is not well documented, assumptions were made based on tests
+    using XTerm.
+
+    *   For a CSI sequence to be considered SGR, it must end with "m" and in
+        between have zero or more integers and semicolons.
+    *   Invalid SGR will lead to one of the following behaviours
+        *   Invalidate the whole sequence skipping all SGR.
+        *   Invalidate only itself, skipping evaluation of that specific SGR.
+    *   Specifying a fg or bg color multiple times will print only the last
+        color definition.
+    *   Specifying an attribute multiple times will combine them unless they
+        revert each other.
+
+    With the definitions above, we set the following pseudo regex:
+
+    -  4-bit SGR: (;*#(;#)*)?m
+    -  8-bit SGR: (4-bit;+)?[3-4]8;5;<0-255>(;+4-bit)*m
+    - 24-bit SGR: (4-bit;+)?[3-4]8;2;<0-255>;<0-255>;<0-255>(;+4-bit)*m
+
+    Parameters
+    ----------
+    extra_colors : Optional[bool]
+        Whether to allow extra colors or not.
+    exclude_colors : Optional[list[str]]
+        Color codes to be excluded.
+
+    Returns
+    -------
+        str: compiled regular expression with negative lookahead
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/ANSI_escape_code
+    .. [2] https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+    .. [3] https://man.archlinux.org/man/console_codes.4.en.txt
+
+    Examples
+    --------
+    Redact unsafe sequences by default:
+    >>> color_pattern = exclude_pattern(r"(3[0-7])", ["30"])
+    '(?!(?:31))3[0-7]'
     """
-    Safely print the text passed as argument based in the allow list:
-      - Printable ASCII + newline + tab
-      - ANSI 4-bit SGR
+
+    ## TODO: verify which attributes should stay.
+    sgr_4bit = r"([0-9]|2[1-5]|2[7-9]|3[0-7]|39|4[0-7]|49|9[0-7]|10[0-7])"
+    if exclude_colors:
+        sgr_4bit = exclude_pattern(sgr_4bit, exclude_colors)
+    sgr_4bit = rf";*{sgr_4bit}(;{sgr_4bit})*"
+    sgr_re = rf"(;*|{sgr_4bit})?m"
+    if extra_colors:
+        eight_bit = r"([0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
+        sgr_8bit = rf"[3-4]8;5;{eight_bit}"
+        sgr_24bit = rf"[3-4]8;2;{eight_bit};{eight_bit};{eight_bit}"
+        sgr_extra = rf"({sgr_8bit}|{sgr_24bit})"
+        if exclude_colors:
+            sgr_extra = exclude_pattern(sgr_extra, exclude_colors)
+        sgr_re = rf"{sgr_re}|({sgr_4bit};+)*;*{sgr_extra};*(;+{sgr_4bit})*m"
+    return re.compile(sgr_re)
+
+
+def safe_print(untrusted_text: str,
+               colors: Optional[bool] = True,
+               extra_colors: Optional[bool] = True,
+               exclude_colors: Optional[list[str]] = None,
+               ) -> str:
+    """Sanitize untrusted text to be printed to the terminal.
+
+    Safely print the text passed as argument based in an allow list, allowing
+    print printable ASCII, newline, tab and a subset of SGR (color and
+    formatting codes).
+
+    Parameters
+    ----------
+    untrusted_text : str
+        The unsafe text to be sanitized.
+    colors : Optional[bool] = True
+        Whether to allow colors or not.
+    extra_colors : Optional[bool] = True
+        Whether to allow extra colors or not.
+    exclude_colors : Optional[list[str]] = None
+        Color codes to be excluded.
+
+    Returns
+    -------
+        str: sanitized text
+
+    Examples
+    --------
+    Redact unsafe sequences by default:
+    >>> safe_print("\x1b[2Jvulnerable: True\b\b\b\bFalse")
+    _[2Jvulnerable: True____False
+
+    Redact all colors:
+    >>> safe_print("\x1b[38;5;0m\x1b[31m\x1b[38;2;0;0;0m", colors=False)
+    _[38;5;0m_[31m_[38;2;0;0;0m
+
+    Allow colors but not extra colors bigger than 4bit:
+    >>> safe_print("\x1b[38;5;0m\x1b[31m\x1b[38;2;0;0;0m", colors=True,
+    ...            extra_colors=False)
+    _[38;5;0m\x1b[31m_[38;2;0;0;0m
     """
-    output = []
-    length = len(text)
 
     allowed_space = {ord("\n"), ord("\t")}
-    ## Warning: text is concealed if fg color is the same as terminal bg color.
-    ## TODO: verify which attributes should stay.
-    ## SGR 4-bit: (;*<format>(;<format>)*m|m)
-    ## SGR 8-bit: ((<4bit>;+)?[3-4]8;5;<0-255>m)
-    ## SGR 24-bit: ((<4bit>;+)?[3-4]8;2;<0-255>;<0-255>;<0-255>m)
     if colors:
-        sgr_4bit = r"([0-9]|2[1-5]|2[7-9]|3[0-7]|39|4[0-7]|49|9[0-7]|10[0-7])"
-        sgr_4bit = exclude_color(sgr_4bit, exclude_colors)
-        sgr_4bit = rf";*{sgr_4bit}(;{sgr_4bit})*"
-        sgr_re = rf"(;*|{sgr_4bit})?m"
-        if extra_colors:
-            eight_bit = r"([0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
-            sgr_8bit = rf"[3-4]8;5;{eight_bit}"
-            sgr_24bit = rf"[3-4]8;2;{eight_bit};{eight_bit};{eight_bit}"
-            sgr_re = rf"{sgr_re}|({sgr_4bit};+)*({sgr_8bit}|{sgr_24bit})m"
-        sgr_pattern = re.compile(sgr_re)
-
+        sgr_pattern = get_color_pattern(extra_colors=extra_colors,
+                                        exclude_colors=exclude_colors)
+    output = []
+    length = len(untrusted_text)
     i = 0
     while i < length:
-        char = text[i]
+        char = untrusted_text[i]
         hex_value = ord(char)
         if 0x20 <= hex_value <= 0x7e or hex_value in allowed_space:
             output.append(char)
-        elif (colors and hex_value == 0x1b and text[i + 1] == "[" and
-              (sgr_match := re.match(sgr_pattern, text[i + 2:]))):
-            ## Sanitize CSI sequence
+        elif (colors and hex_value == 0x1b and i + 1 < len(untrusted_text) and
+              untrusted_text[i + 1] == "[" and
+              (sgr_match := re.match(sgr_pattern, untrusted_text[i + 2:]))):
             sgr_match_length = sgr_match.end()
-            output.append(text[i:i + 2 + sgr_match_length])
+            output.append(untrusted_text[i:i + 2 + sgr_match_length])
             i += sgr_match_length + 1
         else:
-            ## Sanitize prohibited character
             output.append("_")
         i += 1
 
     return "".join(output)
 
 
-def main():
+def main() -> None:
+    """Read untrusted text and sanitize it to be safely printed on the terminal.
+
+    Examples
+    --------
+    From the shell, strings must be provided with escapes already interpreted:
+    $ safe_print.py "$(printf '%b' "untrusted_string\033[2K")"
     """
-    Read text input and sanitize it.
-    You may pass from the terminal:
-      safe_print.py "$(printf '%b\n' "untrusted_string\033[2K")"
-    """
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("text", nargs=argparse.REMAINDER, help="text to print safely")
+    parser.add_argument("untrusted_text", nargs=argparse.REMAINDER,
+                        help="text to print safely")
     args = parser.parse_args()
-    text = "\n".join(args.text)
-    print(safe_print(text))
+    untrusted_text = "\n".join(args.untrusted_text)
+    print(safe_print(untrusted_text))
 
 
 if __name__ == "__main__":
--- safe_print_test.py
+++ safe_print_test.py
@@ -13,7 +13,7 @@ class TestSafePrint(unittest.TestCase):
     """
 
 
-    def test_safe_print_esc(self):
+    def test_safe_print_esc(self) -> None:
         """
         Test ESC sequence
         """
@@ -25,23 +25,20 @@ class TestSafePrint(unittest.TestCase):
             ("\v", "_"),
             ("\f", "_"),
             ("\r", "_"),
-        ]
-        for text, result in cases:
-            with self.subTest(text=text, result=result):
-                self.assertEqual(safe_print(text),
-                                 result)
-
-
-    def test_safe_print_osc(self):
-        """
-        Test OSC sequence
-        """
-        cases = [
+            ("\0", "_"),
+            ("\1", "_"),
+            ("\u0061", "a"),
+            ("\u00D6 or \u00F6", "_ or _"),
+            ("Ö or ö", "_ or _"),
             ("\x1b]8;;", "_]8;;"),
+            ("\u0061", "a"),
             ("a\x1b]8;;b", "a_]8;;b"),
             ("a\x1b]8;;", "a_]8;;"),
             ("a\x1b] 8;;", "a_] 8;;"),
             ("a\x1b ]8;;", "a_ ]8;;"),
+            ("\033", "_"),
+            ("\033[", "_["),
+            ("\x1b[2K", "_[2K"),
         ]
         for text, result in cases:
             with self.subTest(text=text, result=result):
@@ -49,14 +46,22 @@ class TestSafePrint(unittest.TestCase):
                                  result)
 
 
-    def test_safe_print_color(self):
+    def test_safe_print_color(self) -> None:
         """
-        Test with colors
+        Test with colors.
         """
         cases = [
             ("\x1b[31m", "\x1b[31m"),
             ("\x1b[38;5;1m", "\x1b[38;5;1m"),
-            ("\x1b[38;2;255;0;0m", "\x1b[38;2;255;0;0m"),
+            ("\x1b[;38;5;1m", "\x1b[;38;5;1m"),
+            ("\x1b[0;38;5;1m", "\x1b[0;38;5;1m"),
+            ("\x1b[38;5;1;1m", "\x1b[38;5;1;1m"),
+            ("\x1b[38;5;1;m", "\x1b[38;5;1;m"),
+            ("\x1b[38;2;255;0;1m", "\x1b[38;2;255;0;1m"),
+            ("\x1b[38;2;255;0;0;m", "\x1b[38;2;255;0;0;m"),
+            ("\x1b[38;2;255;0;0;0m", "\x1b[38;2;255;0;0;0m"),
+            ("\x1b[;38;2;255;0;0m", "\x1b[;38;2;255;0;0m"),
+            ("\x1b[0;38;2;255;0;0m", "\x1b[0;38;2;255;0;0m"),
         ]
         for text, result in cases:
             with self.subTest(text=text, result=result):
@@ -65,9 +70,9 @@ class TestSafePrint(unittest.TestCase):
                                  result)
 
 
-    def test_safe_print_no_extra_color(self):
+    def test_safe_print_no_extra_color(self) -> None:
         """
-        Test disabling extra colors
+        Test disabling extra colors.
         """
         cases = [
             ("\x1b[31m", "\x1b[31m"),
@@ -81,9 +86,9 @@ class TestSafePrint(unittest.TestCase):
                                  result)
 
 
-    def test_safe_print_no_color(self):
+    def test_safe_print_no_color(self) -> None:
         """
-        Test disabling colors
+        Test disabling colors.
         """
         cases = [
             ("\x1b[31m", "_[31m"),
@@ -97,15 +102,20 @@ class TestSafePrint(unittest.TestCase):
                                  result)
 
 
-    def test_safe_print_no_specific_color(self):
+    def test_safe_print_no_specific_color(self) -> None:
         """
-        Test disabling specific colors
+        Test disabling specific colors.
         """
         cases = [
             ("\x1b[30m", "_[30m"),
             ("\x1b[31m", "\x1b[31m"),
             ("\x1b[32m", "\x1b[32m"),
             ("\x1b[37m", "_[37m"),
+            ("\x1b[30;1m", "_[30;1m"),
+            ("\x1b[0;30m", "_[0;30m"),
+            ("\x1b[38;5;30m", "\x1b[38;5;30m"),
+            ("\x1b[0;;;30;;;38;5;0m", "_[0;;;30;;;38;5;0m"),
+            ("\x1b[38;2;0;30;0m", "\x1b[38;2;0;30;0m"),
         ]
         for text, result in cases:
             with self.subTest(text=text, result=result):
